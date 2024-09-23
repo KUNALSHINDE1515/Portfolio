1c4db99b307704f49c69f76a61dae5125f98a309
KUNALSHINDE1515 <kunalshinde7956@gmail.com>
2024-09-23 16:29:45 +0530

    Porfolio building

---
+++ script.js
@@ -0,0 +1,11 @@
+// const bar = document.querySelector(".fa-bars");
+// const nav = document.querySelector('.nav-container')
+// console.log(nav);
+
+
+
+
+// bar.addEventListener("click" , () => {
+  
+    
+// })
